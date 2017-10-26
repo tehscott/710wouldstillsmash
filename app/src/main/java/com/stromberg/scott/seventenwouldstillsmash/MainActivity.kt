cfa5487dab4e0f69ff102dd1a60ec429cd98856f
Scott Stromberg <sstromberg@SStromberg-Mac.CTAC.1800contacts.com>
2017-10-26 14:56:31 -0600

    starting on statistics

--- app/src/main/java/com/stromberg/scott/seventenwouldstillsmash/MainActivity.kt
+++ app/src/main/java/com/stromberg/scott/seventenwouldstillsmash/MainActivity.kt
@@ -5,17 +5,23 @@ import android.content.res.ColorStateList
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
-import android.view.MotionEvent
 import android.view.View
+import android.widget.Toast
 import com.github.clans.fab.FloatingActionButton
 import com.github.clans.fab.FloatingActionMenu
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.stromberg.scott.seventenwouldstillsmash.fragment.BaseFragment
 import com.stromberg.scott.seventenwouldstillsmash.fragment.CreateGameFragment
 import com.stromberg.scott.seventenwouldstillsmash.fragment.GamesFragment
 import com.stromberg.scott.seventenwouldstillsmash.fragment.PlayersFragment
 import com.stromberg.scott.seventenwouldstillsmash.model.Game
+import com.stromberg.scott.seventenwouldstillsmash.model.Player
 import kotlinx.android.synthetic.main.activity_main.*
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
     private var mCurrentFragment: BaseFragment? = null
@@ -81,6 +87,8 @@ class MainActivity : AppCompatActivity() {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         navigation.selectedItemId = R.id.navigation_games
         navigation.itemTextColor = ColorStateList.valueOf(resources.getColor(R.color.text_secondary))
+
+        updateStatistics()
     }
 
     override fun attachBaseContext(newBase: Context) {
@@ -110,6 +118,77 @@ class MainActivity : AppCompatActivity() {
         }
         else {
             super.onBackPressed()
+        }
+    }
+
+    private fun updateStatistics() {
+        /* todo: save the last game id, pull th emost recent game. if they match, no stats changes (probably)
+        allow force refresh?
+        include last refreshed date
+         */
+
+        Toast.makeText(this@MainActivity, "Updating statistics", Toast.LENGTH_SHORT).show()
+
+        Thread({
+            val db = FirebaseDatabase.getInstance()
+
+            val players = ArrayList<Player>()
+            getPlayers(db, players)
+        }).start()
+    }
+
+    private fun getPlayers(db: FirebaseDatabase, players: ArrayList<Player>) {
+        db.reference
+            .child("players")
+            .orderByKey()
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError?) {}
+
+                override fun onDataChange(snapshot: DataSnapshot?) {
+                    snapshot?.children?.reversed()?.forEach {
+                        val player: Player = it.getValue(Player::class.java)!!
+                        player.id = it.key
+                        players.add(player)
+                    }
+
+                    val games = ArrayList<Game>()
+                    getGames(db, games, players)
+                }
+            })
+    }
+
+    private fun getGames(db: FirebaseDatabase, games: ArrayList<Game>, players: ArrayList<Player>) {
+        db.reference
+            .child("games")
+            .orderByKey()
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError?) {}
+
+                override fun onDataChange(snapshot: DataSnapshot?) {
+                    snapshot?.children?.reversed()?.forEach {
+                        val game: Game = it.getValue(Game::class.java)!!
+                        game.id = it.key
+                        games.add(game)
+                    }
+                }
+            })
+    }
+
+    private fun gameDataFetched(games: ArrayList<Game>, players: ArrayList<Player>) {
+        if(games.size > 0 && players.size > 0) {
+            calculateWinRates(games, players)
+        }
+    }
+
+    private fun calculateWinRates(games: ArrayList<Game>, players: ArrayList<Player>) {
+        players.forEach {
+            var playerId = it.id
+
+            games.forEach {
+                it.players.firstOrNull {
+                    it.player!!.id == playerId
+                }
+            }
         }
     }
 }
